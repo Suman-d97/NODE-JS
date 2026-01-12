7d5e6f5893218a667ec0aedb5816cf4271eb28ca
Suman-da97 <sumandass1811@gmail.com>
2026-01-13 00:50:51 +0530

    Add initial server setup and file handling examples

---
+++ module/fsmodule.js
@@ -0,0 +1,45 @@
+//Create File
+
+// const fs = require('fs');
+// fs.writeFile('demoFile.txt','WellCome to NODE js', function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log('File created successfully');
+//     }
+// });
+
+//Read File
+
+// const fs = require('fs');
+// fs.readFile('demoFile.txt','utf-8',function(err,data){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('Read file: ',data)
+//     }
+// })
+
+//Update File
+
+// const fs = require('fs');
+// fs.appendFile('demoFile.txt',' Hello this is new text',function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('Update data successfully')
+//     }
+// })
+
+//Delete File
+
+// const fs = require('fs');
+// fs.unlink('demoFile.txt',function(err){
+//     if(err){
+//         console.log(err);
+        
+//     }else{
+//         console.log('Delete file successfully');
+        
+//     }
+// })
